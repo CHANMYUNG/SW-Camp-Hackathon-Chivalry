02f604bcb216b2afa1bfa47b5305b4f1b6e45f83
Terry Yun <nooheat1228@gmail.com>
2017-07-27 06:26:35 +0900

    main_detail

--- Chivalry/app/src/main/java/com/example/a10102/chivalry/room/RoomActivity.java
+++ Chivalry/app/src/main/java/com/example/a10102/chivalry/room/RoomActivity.java
@@ -23,7 +23,7 @@ public class RoomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.prize_pain);
+        setContentView(R.layout.main_detail);
 
         int roomNum = Room.getRoomNum();
 
