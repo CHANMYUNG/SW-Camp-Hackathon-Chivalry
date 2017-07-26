e1617da430561978cb23dcc052c84dc38afd27f6
PpalPpaLi <kdgbang@gmail.com>
2017-07-27 00:33:50 +0900

    Create new Reproject

---
+++ Chivalry/app/src/main/java/com/example/a10102/chivalry/ListViewItem.java
@@ -0,0 +1,41 @@
+package com.example.a10102.chivalry;
+
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by dsm2017 on 2017-07-26.
+ */
+
+public class ListViewItem {
+    private Drawable cleanIconDrawable;
+    private Drawable attentionIconDrawable;
+    private String roomStr;
+    private String memberStr;
+
+    public void setCleanIcon(Drawable icon) {
+        cleanIconDrawable = icon;
+    }
+    public void setAttentionIcon(Drawable button) {
+        attentionIconDrawable = button;
+    }
+    public void setRoom(String title) {
+        roomStr = title;
+    }
+    public void setMember(String desc) {
+        memberStr = desc;
+    }
+
+    public Drawable getCleanIcon() {
+        return this.cleanIconDrawable;
+    }
+    public Drawable getAttentionIcon() {
+        return this.attentionIconDrawable;
+    }
+    public String getRoom() {
+        return this.roomStr;
+    }
+    public String getMember() {
+        return this.memberStr;
+    }
+}
