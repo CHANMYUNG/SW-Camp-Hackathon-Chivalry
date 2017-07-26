4e1e1e366d91deab31244eefd9f88453b8f53964
Terry Yun <nooheat1228@gmail.com>
2017-07-27 00:25:23 +0900

    add patrolRouter

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,10 @@ let router = require('express').Router();
 let prizeRouter = require('./prize');
 let painRouter = require('./pain');
 let roomRouter = require('./room');
-
+let patrolRouter = require('./patrol');
 
 router.use('/', prizeRouter);
 router.use('/', painRouter);
 router.use('/', roomRouter);
-
+router.use('/', patrolRouter);
 module.exports = router;
