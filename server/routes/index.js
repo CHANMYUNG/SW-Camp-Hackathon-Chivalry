930b70dd50a25ed6eb7e0867ac5d589a842a2f1b
Terry Yun <nooheat1228@gmail.com>
2017-07-27 01:53:38 +0900

    특정 학생의 정보를 가져오는 기능 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ let prizeRouter = require('./prize');
 let painRouter = require('./pain');
 let roomRouter = require('./room');
 let patrolRouter = require('./patrol');
+let studentRouter = require('./student');
 
 router.use('/', prizeRouter);
 router.use('/', painRouter);
 router.use('/', roomRouter);
 router.use('/', patrolRouter);
+router.use('/', studentRouter);
 module.exports = router;
---
+++ server/routes/student/index.js
@@ -0,0 +1,21 @@
+let router = require('express').Router();
+
+router.route('/student/:stuNum').get(function (req, res) {
+    let db = req.app.get('db');
+    let stuNum = req.params.stuNum;
+    db.query("SELECT * FROM STUDENT WHERE stuNum = ?", stuNum, function (err, result) {
+        if (err) res.status(500).end();
+        else if (result.length === 1) {
+            let response = {
+                "stuNum": result[0].stuNum,
+                "name": result[0].name,
+                "prize": result[0].prize,
+                "pain": result[0].pain
+            }
+            res.status(200).json(response);
+        } else res.status(500).end();
+    });
+});
+
+
+module.exports = router;
