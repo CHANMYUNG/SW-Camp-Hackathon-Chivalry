063ae5cbc4f2086139a9ecc858c6eb2b77345918
Xsolute <Xsolute01@gmail.com>
2017-07-26 19:32:41 +0900

    create main list view
    create main

---
+++ MyApplication/app/src/main/java/com/example/a10102/myapplication/ListViewAdapter.java
@@ -0,0 +1,44 @@
+package com.example.a10102.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dsm2017 on 2017-07-26.
+ */
+
+public class ListViewAdapter extends BaseAdapter {
+    // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
+    private ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>();
+
+    // ListviewAdapter의 생성자
+    public ListViewAdapter() {}
+
+    // Adapter에 사용되는 데이터의 개수를 리턴 : 필수 구현
+    @Override
+    public int getCount() {
+        return listViewItemList.size();
+    }
+
+    // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴 : 필수 구현
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
+
+        // "listview_item" Layout을 inflate하여 convertView 참조 획득
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.listview_item, parent, false);
+        }
+
+        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+        ImageView iconImageView = (ImageView) convertView.findViewById(R.id.imageview1);
+    }
+}
--- MyApplication/app/src/main/java/com/example/a10102/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/a10102/myapplication/MainActivity.java
@@ -12,9 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // ListView
+    static final String[] ListMenu = {"301호", "302호", "303호"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ListMenu);
+
+        ListView listview = (ListView) findViewById(R.id.roomlistView);
+        listview.setAdapter(adapter);
+
+
     }
 
     @Override
